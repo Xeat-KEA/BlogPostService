aa07c56a489b35e2c518be39a8d1c0c2447bb738
starboxxxx <leegd120@gmail.com>
2024-10-10 21:42:43 +0900

    fix : add @RequestBody annotation

--- src/main/java/xeat/blogservice/article/controller/ArticleController.java
+++ src/main/java/xeat/blogservice/article/controller/ArticleController.java
@@ -2,6 +2,7 @@ package xeat.blogservice.article.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import xeat.blogservice.article.dto.ArticlePostRequestDto;
 import xeat.blogservice.article.entity.Article;
@@ -15,7 +16,7 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @PostMapping("/blog/board/article")
-    public Response<Article> postArticle(ArticlePostRequestDto articlePostRequestDto) {
+    public Response<Article> postArticle(@RequestBody ArticlePostRequestDto articlePostRequestDto) {
         return articleService.post(articlePostRequestDto);
     }
 }
