490924e5d871dca84baf2808b2424170c897900d
starboxxxx <leegd120@gmail.com>
2024-10-21 00:12:38 +0900

    fix : add notice save feature on replyService

--- src/main/java/xeat/blogservice/reply/service/ReplyService.java
+++ src/main/java/xeat/blogservice/reply/service/ReplyService.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import xeat.blogservice.article.repository.ArticleRepository;
+import xeat.blogservice.blog.entity.Blog;
 import xeat.blogservice.blog.repository.BlogRepository;
 import xeat.blogservice.global.Response;
+import xeat.blogservice.notice.entity.Notice;
+import xeat.blogservice.notice.entity.NoticeCategory;
+import xeat.blogservice.notice.repository.NoticeRepository;
 import xeat.blogservice.reply.dto.ReplyPostRequestDto;
 import xeat.blogservice.reply.dto.ReplyPostResponseDto;
 import xeat.blogservice.reply.entity.Reply;
@@ -21,6 +25,8 @@ public class ReplyService {
 
     private final BlogRepository blogRepository;
 
+    private final NoticeRepository noticeRepository;
+
     @Transactional
     public Response<ReplyPostResponseDto> replyPost(ReplyPostRequestDto replyPostRequestDto) {
 
@@ -31,6 +37,22 @@ public class ReplyService {
                 .content(replyPostRequestDto.getContent())
                 .build();
         replyRepository.save(reply);
+
+        // 블로그 알림 상태 확인 false로 업데이트
+        Blog blog = blogRepository.findById(reply.getArticle().getBlog().getId()).get();
+        blog.updateNoticeCheckFalse();
+        blogRepository.save(blog);
+
+
+        // 알림 table에 추가
+        Notice notice = Notice.builder()
+                .blog(blog)
+                .sentUser(reply.getUser())
+                .noticeCategory(NoticeCategory.REPLY)
+                .content(reply.getContent())
+                .build();
+
+        noticeRepository.save(notice);
 
         return Response.success(ReplyPostResponseDto.toDto(reply));
     }
