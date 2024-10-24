05c22de8e63acb55ca6e110cb964773df0bed4b9
starboxxxx <leegd120@gmail.com>
2024-10-24 23:40:28 +0900

    fix : ArticleResponseDto 인터페이스 생성 및 상속 관계 설정

--- src/main/java/xeat/blogservice/codearticle/dto/CodeArticleRecentResponseDto.java
+++ src/main/java/xeat/blogservice/codearticle/dto/CodeArticleRecentResponseDto.java
@@ -3,6 +3,7 @@ package xeat.blogservice.codearticle.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import xeat.blogservice.article.dto.ArticleResponseDto;
 import xeat.blogservice.codearticle.entity.CodeArticle;
 
 import java.time.LocalDateTime;
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class CodeArticleRecentResponseDto {
+public class CodeArticleRecentResponseDto implements ArticleResponseDto {
 
     private Long articleId;
 
