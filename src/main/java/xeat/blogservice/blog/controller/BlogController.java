abf7bab5fbba24b85946c925fe51a5e626225c35
starboxxxx <leegd120@gmail.com>
2024-10-27 21:27:07 +0900

    fix : 깃 충돌로 인한 파일 수정

--- src/main/java/xeat/blogservice/blog/controller/BlogController.java
+++ src/main/java/xeat/blogservice/blog/controller/BlogController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import xeat.blogservice.blog.dto.BlogCreateRequestDto;
 import xeat.blogservice.blog.dto.BlogEditRequestDto;
 import xeat.blogservice.blog.dto.BlogNoticeCheckResponseDto;
+import xeat.blogservice.blog.dto.BlogMainContentResponseDto;
 import xeat.blogservice.blog.entity.Blog;
 import xeat.blogservice.blog.service.BlogService;
 import xeat.blogservice.global.Response;
@@ -18,6 +19,11 @@ public class BlogController {
     @GetMapping("/notice/check/{blogId}")
     public Response<BlogNoticeCheckResponseDto> getNoticeCheck(@PathVariable Long blogId) {
         return blogService.getNoticeCheck(blogId);
+    }
+
+    @GetMapping("/mainContent/{blogId}")
+    public Response<BlogMainContentResponseDto> getMainContent(@PathVariable Long blogId) {
+        return blogService.getMainContent(blogId);
     }
 
     @PostMapping("/blog/create")
--- src/main/java/xeat/blogservice/blog/service/BlogService.java
+++ src/main/java/xeat/blogservice/blog/service/BlogService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import xeat.blogservice.blog.dto.BlogCreateRequestDto;
 import xeat.blogservice.blog.dto.BlogEditRequestDto;
 import xeat.blogservice.blog.dto.BlogNoticeCheckResponseDto;
+import xeat.blogservice.blog.dto.BlogMainContentResponseDto;
 import xeat.blogservice.blog.entity.Blog;
 import xeat.blogservice.blog.repository.BlogRepository;
 import xeat.blogservice.global.Response;
@@ -15,6 +16,12 @@ import xeat.blogservice.global.Response;
 public class BlogService {
 
     private final BlogRepository blogRepository;
+
+    @Transactional
+    public Response<BlogMainContentResponseDto> getMainContent(Long blogId) {
+        Blog blog = blogRepository.findById(blogId).get();
+        return Response.success(BlogMainContentResponseDto.toDto(blog));
+    }
 
     @Transactional
     public Response<BlogNoticeCheckResponseDto> getNoticeCheck(Long blogId) {
