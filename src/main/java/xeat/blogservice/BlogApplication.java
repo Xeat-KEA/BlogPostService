d0319e68d67f8c2b2eabd5bfb10aa2779e12f100
starboxxxx <leegd120@gmail.com>
2024-10-30 02:31:03 +0900

    CT-137 feat : eureka 클라이언트 등록 및 설정 파일 변경

--- src/main/java/xeat/blogservice/BlogApplication.java
+++ src/main/java/xeat/blogservice/BlogApplication.java
@@ -4,13 +4,16 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableDiscoveryClient
 @OpenAPIDefinition(servers = {
 		@Server(url = "/", description = "Default Server URL")
 })
+
 public class BlogApplication {
 
 	public static void main(String[] args) {
