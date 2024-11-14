ad2d143dd0df5fdbb8d5ab9094c266c637057bed
starboxxxx <leegd120@gmail.com>
2024-11-14 13:58:17 +0900

    test 제거

--- src/test/java/xeat/blogservice/BlogApplicationTests.java
+++ src/test/java/xeat/blogservice/BlogApplicationTests.java
@@ -3,11 +3,11 @@ package xeat.blogservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BlogApplicationTests {
+//@SpringBootTest
+//class BlogApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
-}
+//}
