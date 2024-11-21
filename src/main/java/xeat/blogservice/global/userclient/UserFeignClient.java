7bd293f869966599602ebbbcf2fd84ff296a7ca9
starboxxxx <leegd120@gmail.com>
2024-11-21 10:36:54 +0900

    @FeignClient 추가 설정

--- src/main/java/xeat/blogservice/global/userclient/UserFeignClient.java
+++ src/main/java/xeat/blogservice/global/userclient/UserFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "userservice-dev", url = "http://172.16.211.57:8081")
+@FeignClient(name = "user-service")
 public interface UserFeignClient {
 
     @GetMapping(value = "/users/info")
