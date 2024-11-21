f678a670e92e258cdc7c8ddfeed909f77b0027ca
starboxxxx <leegd120@gmail.com>
2024-11-21 09:59:53 +0900

    userInfo url 수정2

--- src/main/java/xeat/blogservice/global/userclient/UserFeignClient.java
+++ src/main/java/xeat/blogservice/global/userclient/UserFeignClient.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(name = "userservice-dev", url = "http://172.16.211.57:8081")
 public interface UserFeignClient {
 
-    @GetMapping(value = "/users/userInfo")
+    @GetMapping(value = "/users/info")
     UserInfoResponseDto getUserInfo(@RequestHeader("UserId") String userId);
 }
