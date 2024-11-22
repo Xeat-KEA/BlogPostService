328984e24d91360c355c49cdd7f967619704a82f
starboxxxx <leegd120@gmail.com>
2024-11-22 19:38:57 +0900

    fix : feign client 수정

--- src/main/java/xeat/blogservice/global/feignclient/UserFeignClient.java
+++ src/main/java/xeat/blogservice/global/feignclient/UserFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "user-service", url = "http://172.16.211.57:44501")
+@FeignClient(name = "user-service", url = "http://172.16.211.57:19091")
 public interface UserFeignClient {
 
     @GetMapping(value = "/user-service/users/info")
