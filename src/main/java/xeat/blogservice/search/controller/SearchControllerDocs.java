bff9dedbb32f86ef96b55d1462c7806c5856b22a
gytjd_omk5aqq <gytjd9516@naver.com>
2024-12-11 22:07:09 +0900

    docs: 게시글검색 api 스웨거 수정

--- src/main/java/xeat/blogservice/search/controller/SearchControllerDocs.java
+++ src/main/java/xeat/blogservice/search/controller/SearchControllerDocs.java
@@ -20,7 +20,7 @@ public interface SearchControllerDocs {
 
     @Operation(summary = "게시글 검색", description = "ex) /blog/article/search?query=자바&type=all&sort=score&page=0")
     @Parameters(value = {@Parameter(name = "query", description = "사용자가 입력한 검색어, 입력 안하면 검색 결과 비어있음", in = ParameterIn.QUERY),
-            @Parameter(name = "type", description = "검색할 게시글 유형", required = true, in = ParameterIn.QUERY),
+            @Parameter(name = "type", description = "검색할 게시글 유형 all, normal(일반게시글만), code(코테게시글만)", required = true, in = ParameterIn.QUERY),
             @Parameter(name = "sort", description = "score=연관도순, latest=최신순", required = true, in = ParameterIn.QUERY),
             @Parameter(name = "page", description = "페이지번호, 0부터 시작", required = true, in = ParameterIn.QUERY)}
     )
