72c96837e5479799247c80abbeac029424fbcb55
gytjd_omk5aqq <gytjd9516@naver.com>
2024-12-12 20:51:37 +0900

    fix: 게시글 검색에 카테고리 id 추가

--- src/main/java/xeat/blogservice/search/entity/ElasticArticle.java
+++ src/main/java/xeat/blogservice/search/entity/ElasticArticle.java
@@ -33,6 +33,10 @@ public class ElasticArticle {
     private Integer commentCount;
     @Field(name="view_count")
     private Integer viewCount;
+    @Field(name="child_category_id")
+    private Long child_category_id;
+    @Field(name="parent_category_id")
+    private Long parent_category_id;
 
     public ElasticArticle() {
 
